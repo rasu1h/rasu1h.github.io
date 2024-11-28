c450249bc7302cee47502229c0da902b71307b31
Rassul <rasa.laik@gmail.com>
2024-11-28 23:02:50 +0500

    Objecting the speaks and speakWord

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -11,8 +11,8 @@
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-var speakWord = "Good Bye";
 var ByeName = {};
+ByeName.speakWord = "Good Bye";
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
