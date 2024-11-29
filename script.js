e746fee01a8731adf58b1b4a6450f483ed01693c
Rassul <rasa.laik@gmail.com>
2024-11-29 22:41:42 +0500

    Module 5 final

--- script.js
+++ script.js
@@ -1,68 +1,56 @@
-// *******************************
-// START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
-// *******************************
-//
-// Module 4 Assignment Instructions.
-//
-// The idea of this assignment is to take an existing array of names
-// and then output either Hello 'Name' or Good Bye 'Name' to the console.
-// The program should say "Hello" to any name except names that start with a "J"
-// or "j", otherwise, the program should say "Good Bye". So, the final output
-// on the console should look like this:
-/*
-Hello Yaakov
-Good Bye John
-Good Bye Jen
-Good Bye Jason
-Hello Paul
-Hello Frank
-Hello Larry
-Hello Paula
-Hello Laura
-Good Bye Jim
+document.querySelector('.menu-toggle').addEventListener('click', function () {
+  document.querySelector('.mobile-menu').classList.toggle('active');
+});
 
-WARNING!!! WARNING!!!
-The code does NOT currently work! It is YOUR job to make it work
-as described in the requirements and the steps in order to complete this
-assignment.
-WARNING!!! WARNING!!!
+(function (global) {
+  var dc = {};
 
-*/
+  // TODO: ШАГ 1 — список категорий
+  var categories = ['chicken.html', 'beef.html', 'sushi.html']; // Категории для случайного выбора
 
-// STEP 1:
-// Wrap the entire contents of script.js inside of an IIFE
-// See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
-
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-// STEP 10:
-// Loop over the names array and say either 'Hello' or "Good Bye"
-// using either the helloSpeaker's or byeSpeaker's 'speak' method.
-// See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */ let name in names) {
-
-  // STEP 11:
-  // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
-  // names that start with either upper case or lower case 'J'/'j', call
-  // string object's 'toLowerCase' method on the result so we can compare
-  // to lower case character 'j' afterwards.
-  // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
-
-  // STEP 12:
-  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-  // name in the loop.
-  let firstLetter = names[name].charAt(0).toLowerCase();
-  if (/* fill in condition here */ firstLetter === 'j') {
-    // byeSpeaker.xxxx
-   ByeName.speak(names[name]);
-  } else {
-    HelloName.speak(names[name]);
-    // helloSpeaker.xxxx
+  // TODO: ШАГ 2 — функция для выбора случайной категории
+  function getRandomCategory() {
+    var index = Math.floor(Math.random() * categories.length);
+    return categories[index];
   }
 
-}
+  // TODO: ШАГ 3 — загружаем HTML для выбранной категории и заменяем содержимое контейнера
+  dc.loadMenuItems = function (categoryShortName) {
+    console.log('Загружаем меню для категории:', categoryShortName);
+
+    // Создаем новый XMLHttpRequest объект
+    var xhr = new XMLHttpRequest();
+
+    // Открываем запрос на загрузку HTML-файла
+    xhr.open('GET', categoryShortName, true);
+
+    // Обработчик успешного завершения запроса
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        // Если запрос прошел успешно, заменяем содержимое контейнера
+        document.querySelector('.container').innerHTML = xhr.responseText;
+      } else {
+        // Если произошла ошибка, выводим в консоль сообщение
+        console.error('Не удалось загрузить файл: ' + categoryShortName);
+      }
+    };
+
+    // Обработчик ошибки запроса
+    xhr.onerror = function () {
+      console.error('Ошибка при загрузке файла: ' + categoryShortName);
+    };
+
+    // Отправляем запрос
+    xhr.send();
+  };
+
+  // TODO: ШАГ 4 — переопределить обработку плитки "Специальное предложение"
+  document.querySelector('#specials').addEventListener('click', function (event) {
+    event.preventDefault();
+    var randomCategory = getRandomCategory();
+    dc.loadMenuItems(randomCategory); // Загружаем случайную категорию
+  });
+
+  // TODO: ШАГ 5 — подключаем глобальную функцию
+  global.$dc = dc;
+})(window);
